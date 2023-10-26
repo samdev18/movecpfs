739d4b886df4c2cb7fb4202eaeeaff3e12bcdb0d
Samuel Moreira da Silva <40631024+samdev18@users.noreply.github.com>
2023-10-26 17:05:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-const fs = require('fs');
-const path = require('path');
-const xlsx = require('xlsx');
-const yargs = require('yargs');
+import { createReadStream, createWriteStream } from 'fs';
+import { promises as fsp } from 'fs';
+import path from 'path';
+import xlsx from 'xlsx';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import ProgressBar from 'progress';
+import chalk from 'chalk';
 
 function normalizeCPF(cpf) {
-  return cpf.replace(/[^0-9]/g, '');
+  return cpf?.replace(/[^0-9]/g, '');
 }
 
-function moveFilesFromSpreadsheet({
+async function isDirectory(filePath) {
+  const stats = await fsp.stat(filePath);
+  return stats.isDirectory();
+}
+
+async function copyDirectoryRecursive(sourceDir, destDir) {
+  try {
+    await fsp.access(destDir);
+  } catch {
+    await fsp.mkdir(destDir);
+  }
+
+  const entries = await fsp.readdir(sourceDir);
+  for (const entry of entries) {
+    const fullPathSource = path.join(sourceDir, entry);
+    const fullPathDest = path.join(destDir, entry);
+
+    if (await isDirectory(fullPathSource)) {
+      await copyDirectoryRecursive(fullPathSource, fullPathDest);
+    } else {
+      await new Promise((resolve, reject) => {
+        const readStream = createReadStream(fullPathSource);
+        const writeStream = createWriteStream(fullPathDest);
+
+        readStream.pipe(writeStream);
+
+        readStream.on('error', err => reject(err));
+        writeStream.on('error', err => reject(err));
+        writeStream.on('finish', resolve);
+      });
+    }
+  }
+}
+
+async function moveFilesFromSpreadsheet({
   nomeDaPlanilha,
   nomeDaPastaComArquivos,
   nomeDaNovaPasta
@@ -16,34 +54,54 @@ function moveFilesFromSpreadsheet({
   const sheet_name_list = workbook.SheetNames;
   const xlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
-  if (!fs.existsSync(nomeDaNovaPasta)) {
-    fs.mkdirSync(nomeDaNovaPasta);
+  try {
+    await fsp.access(nomeDaNovaPasta);
+  } catch {
+    await fsp.mkdir(nomeDaNovaPasta);
   }
 
-  console.info("movendo arquivos")
-
-  xlData.forEach(row => {
-    console.log(row)
-    const normalizedFileNameFromSheet = normalizeCPF(row["Arquivos"].toString());
-
-    const matchedFile = fs.readdirSync(nomeDaPastaComArquivos).find(file => {
-      const normalizedFileName = normalizeCPF(path.basename(file, path.extname(file)));
-      return normalizedFileName === normalizedFileNameFromSheet;
-    });
-
-    if (matchedFile) {
-      const sourceFilePath = path.join(nomeDaPastaComArquivos, matchedFile);
-      const destFilePath = path.join(nomeDaNovaPasta, matchedFile);
-
-      fs.copyFileSync(sourceFilePath, destFilePath);
-    } else {
-      console.log(`Arquivo correspondente ao CPF ${row["Arquivos"]} não encontrado.`);
-    }
+  const bar = new ProgressBar(':bar :percent :etas', {
+    total: xlData.length,
+    width: 40
   });
-  console.log("Arquivos movidos com sucesso!");
+
+  console.log(chalk.white.bgBlue.bold('Iniciando cópia de arquivos!'));
+
+  const files = await fsp.readdir(nomeDaPastaComArquivos);
+
+  for (const row of xlData) {
+    const normalizedFileNameFromSheet = normalizeCPF(row["Arquivos"]);
+    const matchedEntry = files.find(file => normalizeCPF(path.basename(file, path.extname(file))) === normalizedFileNameFromSheet);
+
+    if (matchedEntry) {
+      const fullPathSource = path.join(nomeDaPastaComArquivos, matchedEntry);
+      const fullPathDest = path.join(nomeDaNovaPasta, matchedEntry);
+
+      if (await isDirectory(fullPathSource)) {
+        await copyDirectoryRecursive(fullPathSource, fullPathDest);
+      } else {
+        await new Promise((resolve, reject) => {
+          const readStream = createReadStream(fullPathSource);
+          const writeStream = createWriteStream(fullPathDest);
+
+          readStream.pipe(writeStream);
+
+          readStream.on('error', err => reject(err));
+          writeStream.on('error', err => reject(err));
+          writeStream.on('finish', resolve);
+        });
+      }
+      bar.tick();
+    } else {
+      console.log(chalk.red(`Entry matching CPF ${row["Arquivos"]} not found.`));
+      bar.tick();
+    }
+  }
+
+  console.log('All files and directories have been copied.');
 }
 
-const argv = yargs
+const argv = yargs(hideBin(process.argv))
   .option('nomeDaPlanilha', {
     description: 'Nome do arquivo da planilha',
     alias: 'p',
@@ -68,4 +126,6 @@ moveFilesFromSpreadsheet({
   nomeDaPlanilha: argv.nomeDaPlanilha,
   nomeDaPastaComArquivos: argv.nomeDaPastaComArquivos,
   nomeDaNovaPasta: argv.nomeDaNovaPasta
+}).catch(error => {
+  console.error('An error occurred:', error);
 });
